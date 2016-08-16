cfb7f5cd7f0a19f72eef86a0771602d1537e9619
Htet Naing Aung <justin.aung19@gmail.com>
2016-08-17 00:28:53 +0700

    add complete table

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -3,10 +3,29 @@
 
 <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title> Regulatory Assessment Tool </title>
 
     <!--Load style.css file, which store in css folder.-->
     <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>css/style.css">
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.12.0/themes/base/jquery-ui.css">
+    <link rel="stylesheet" href="/resources/demos/style.css">
+    <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+    <script src="https://code.jquery.com/ui/1.12.0/jquery-ui.js"></script>
+    <script>
+        $( function() {
+          $( document ).tooltip({
+              track: true
+           });
+        } );
+    </script>
+    <style>
+    label {
+      display: inline-block;
+      width: 5em;
+    }
+    </style>
+
 </head>
 
 <body>
